a18ff9818f8b3a404aa15a389cb33cf656b24364
Efrain Hernandez <efrainhg2050@gmail.com>
2022-02-18 18:54:33 -0600

    CRUD file js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { init } from "./js/usuarios-page";
 import { obtenerChiste, obtenerUsuario } from "./js/http-provider";
-
+import * as CRUD from './js/crud.js';
 //obtenerChiste().then( console.log );
 //obtenerUsuario().then( console.table ).catch( console.error );
 
-init();
+//init();
+
+CRUD.getUsuario(2).then( console.log );
