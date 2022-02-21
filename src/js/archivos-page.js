3d12e144d94d1aa2d684013bac394ef2bd51b809
Efrain Hernandez <efrainhg2050@gmail.com>
2022-02-21 11:53:01 -0600

    Files upload page

---
+++ src/js/archivos-page.js
@@ -0,0 +1,16 @@
+const body = document.body;
+let inputFile = imgFoto;
+
+const crearInputFileHtml = () => {
+    const html = `
+    <h1 class"mt-5">Subir achivos</h1>
+    <hr/>
+
+    <label>Selecciona una fotografia</label>
+    `;
+}
+
+
+export const init = () => {
+    crearInputFileHtml();
+};
